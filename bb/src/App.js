54c4ddfccfb3670e465554ed2da936a2852070d6
AmitKuMaurya <amit69maurya69@gmail.com>
2022-08-25 23:50:23 +0530

    day2

--- bb/src/App.js
+++ bb/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import MiddleNavbar from './Components/MiddleNavbar';
+import UpperNavbar from './Components/UpperNavbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello you are on app</h1>
-      
+      <UpperNavbar/>
+      <MiddleNavbar/>
     </div>
   );
 }
---
+++ bb/src/Components/MiddleNavbar.jsx
@@ -0,0 +1,13 @@
+export default function MiddleNavbar(){
+    return (
+        <div >
+            <div>Home</div>
+            <div>Contact</div>
+            <div>About US</div>
+            <div>Information</div>
+            <div>Blog</div>
+            <div>Settting</div>
+            <div>Coffee with me !</div>
+        </div>
+    )
+}
